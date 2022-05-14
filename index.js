eb27f130fbade67348eedac52aebed5a9f6f5b9f
Danny Lakhisaran <86603@glr.nl>
2022-05-14 19:48:29 +0200

    bijgewerkt 2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js")
 require("dotenv").config()
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -16,6 +18,16 @@ client.on("messageCreate", (message) => {
     if (message.content == "hoi"){
         message.reply("Je bent niet alleen!")
     }
+})
+
+const welcomeChannelId = "975045729687068672"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welkom in de server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
